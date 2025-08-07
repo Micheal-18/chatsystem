f11123d30b7b93985a2724d174d8e832bd2639aa
Micheal-18 <michaeleleke259@gmail.com>
2025-08-07 10:42:30 +0100

    changes in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,10 +6,10 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: "chat-system-dfb04",
-  storageBucket: "chat-system-dfb04.firebasestorage.app",
-  messagingSenderId: "986797118625",
-  appId: "1:986797118625:web:7cff32299ade0be2218ce0"
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
